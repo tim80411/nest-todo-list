2715980c8a18614754a7f52d96e22b5f9ca5f486
Timothy Liao <s446424@sinyi.com.tw>
2023-03-30 11:17:21 +0800

    feat: learning about provider

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('MessageFactory') private readonly name: string,
+    @Inject('Value') private readonly value: { name: string },
+    @Inject('Alias') private readonly alias: AppService,
+    @Inject('Laptop') private readonly computer,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    console.log(this.name);
+    console.log(this.appService === this.alias);
+    console.log(this.computer);
+    return this.appService.getHello() + this.name + this.value.name;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,51 @@ import { AppService } from './app.service';
 import { TodoModule } from './features/todo/todo.module';
 import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
 
+class MessageBox {
+  private readonly message: string;
+  constructor(message: string) {
+    this.message = message;
+  }
+}
+
+class Mac {
+  cpu = 'M1';
+}
+
+class HP {
+  cpu = 'i7';
+}
+
 @Module({
   imports: [TodoModule, CopyTodoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'Name',
+      useValue: 'Tim',
+    },
+    {
+      provide: 'MessageFactory',
+      useFactory(appService: AppService) {
+        return new MessageBox(appService.getHello());
+      },
+      inject: [AppService],
+    },
+    {
+      provide: 'Value',
+      useValue: {
+        name: 'handsome',
+      },
+    },
+    {
+      provide: 'Alias',
+      useExisting: AppService,
+    },
+    {
+      provide: 'Laptop',
+      useClass: false ? Mac : HP,
+    }
+  ],
 })
 export class AppModule {}
